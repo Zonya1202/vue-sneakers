69967169026ba2539277afa80ef4b9856c60cd86
Zonya1202 <ilya-zon@mail.ru>
2024-01-25 19:30:44 +0300

    first commit

---
+++ src/components/CardList.vue
@@ -0,0 +1,25 @@
+<script setup>
+import Card from './Card.vue'
+
+defineProps({
+  items: Array
+})
+
+const emits = defineEmits(['addToFavorite', 'addToCart'])
+</script>
+
+<template>
+  <div class="grid grid-cols-4 gap-5">
+    <Card
+      :key="item.key"
+      v-for="item in items"
+      :title="item.title"
+      :image-url="item.imageUrl"
+      :price="item.price"
+      :is-favorite="item.isFavorite"
+      :is-added="item.isAdded"
+      :on-click-favorite="() => emits('addToFavorite', item)"
+      :on-click-add="() => emits('addToCart', item)"
+    />
+  </div>
+</template>
